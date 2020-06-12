19dc498971f98b2b3ad5cdb888cde8eb8845ee95
Vadim <58609940+vadimsmerekooo@users.noreply.github.com>
2020-06-12 22:47:16 +0200

    Add files via upload

---
+++ AnimationTXT.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace AnimationText
+{
+    /// <summary>
+    /// Логика взаимодействия для AnimationTXT.xaml
+    /// </summary>
+    public partial class AnimationTXT : UserControl
+    {
+        public AnimationTXT()
+        {
+            InitializeComponent();
+        }
+    }
+}
